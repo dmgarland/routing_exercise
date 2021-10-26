e7c66f05878f82ae0911146933e52135e5e6b3da
Dan Garland <dan@dangarland.co.uk>
2021-10-26 10:19:58 +0100

    Update routing_example.rb

--- app/routing_example.rb
+++ app/routing_example.rb
@@ -2,6 +2,6 @@ require 'active_record'
 
 class RoutingExample
   def call(env)
-    [200, {"Content-Type" => "text/html"}, File.open(File.dirname(__FILE__) + '/../public/form.html')]
+    [200, {"Content-Type" => "text/html"}, File.open(File.dirname(__FILE__) + '/../public/index.html')]
   end
 end
